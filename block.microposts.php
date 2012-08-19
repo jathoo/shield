1fd11aa2aa4c59c8c9d68335e66a82e855cdcaa7
Morgante Pell <morgante.pell@morgante.net>
2012-08-19 16:25:30 -0400

    add micropost block

---
+++ block.microposts.php
@@ -0,0 +1,14 @@
+<?php if ( !defined( 'HABARI_PATH' ) ) { die( 'No direct access' ); } ?>
+<div class="module recent microposts">
+	<h3><?php echo $content->title; ?></h3>
+	<a class="feed" href="<?php echo $content->feed; ?>" title="Atom feed of latest microposts"><?php echo _t( 'Feed' ); ?></a>
+	<ol class="items posts">
+	<?php
+	foreach( $content->posts as $post): ?>
+		<li class="item">
+			<p class="content"><?php echo $post->content_out; ?></p>
+			<a class="permalink" href="<?php echo $post->permalink; ?>" title="<?php printf(_t('Posted at %1$s'), $post->pubdate->get('g:i a \o\n F jS, Y')); ?>"><?php echo $post->pubdate->get('M j @ G:i'); ?></a>
+		</li>
+	<?php endforeach; ?>
+	</ol>
+</div>
